7394a7ce58ecc5a2f0da8bda2b34fb9fab905605
Malcolm Childs <michilds0@gmail.com>
2019-04-22 15:30:30 -0400

    Setup docker for local development

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+const { prisma } = require('./generated/prisma-client');
+
+async function main() {
+    const newShoe = await prisma.createShoe({})
+}
+
+
+// const { GraphQLServer } = require('graphql-yoga');
+// const typeDefs = require('./src/schema');
+// const resolvers = require('./src/resolvers');
+
+// const PORT = process.env.PORT || 3000;
+
+// const server = new GraphQLServer({
+//     typeDefs: './src/schema.graphql',
+//     resolvers
+// });
+
+// server.start({ port: PORT }, () => {
+//     console.log(`======================================`);
+//     console.log(`||     Server is Ready!! 😎👍✔      ||`);
+//     console.log('||      Running on port 3000        ||');
+//     console.log(`======================================`);
+// });
+
+
 //const { ApolloServer } = require('apollo-server');
-const { GraphQLServer } = require('graphql-yoga');
-const typeDefs = require('./src/schema');
-const resolvers = require('./src/resolvers');
-
-const PORT = process.env.PORT || 3000;
-
-const server = new GraphQLServer({
-    typeDefs: './src/schema.graphql',
-    resolvers
-});
-
-server.start({ port: PORT }, () => {
-    console.log(`======================================`);
-    console.log(`||     Server is Ready!! 😎👍✔      ||`);
-    console.log('||      Running on port 3000        ||');
-    console.log(`======================================`);
-});
 
 // const express = require('express');
 // const graphqlHTTP = require('express-graphql');
